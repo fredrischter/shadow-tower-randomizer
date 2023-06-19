223fd83ad7a1f706d59e94f7af23fa7f814020b2
fredrischter <fredrischter@gmail.cmo>
2023-06-19 18:20:16 +0200

    finished full walk try, output to walk.txt

---
+++ map_walker.js
@@ -0,0 +1,223 @@
+
+console.log("Mapwalker");
+
+var areas=[
+	{name:"solitary", exits: [
+		{id:"1", dest:"hidden"},
+		{id:"2", dest:"hidden"}
+	]},
+	{name:"hidden", exits: [
+		{id:"1", dest:"solitary"},
+		{id:"2", dest:"solitary"},
+		{id:"3", dest:"forgotten"},
+		{id:"4", dest:"cursed"}
+	]},
+	{name:"forgotten", exits: [
+		{id:"3", dest:"hidden"},
+		{id:"5", dest:"cursed"}
+	]},
+	{name:"cursed", exits: [
+		{id:"4", dest:"hidden"},
+		{id:"5", dest:"forgotten"}
+	]},
+]
+var currentArea = "solitary";
+var enteredFromId;
+
+console.log("map " + JSON.stringify(areas));
+console.log("starting from " + currentArea);
+console.log();
+
+var walkPath = [];
+var knownPaths = {};//["origin"-"destination": [{"dest":"hidden", "id":"2"}]]
+
+var mapsWithKnownUndiscoveredWays = []; // {name:"forgotten", exits: [{id:"1", dest:"hidden"},{id:"2", dest:"cursed"}]
+var desiredDestination = null;
+
+function getKnownUndiscoveredWaysByName(name) {
+	for (var i in mapsWithKnownUndiscoveredWays) {
+		if (mapsWithKnownUndiscoveredWays[i].name == name) {
+			console.log("  from " + name + " - unknown ways to go: " +JSON.stringify(mapsWithKnownUndiscoveredWays[i]));
+			return mapsWithKnownUndiscoveredWays[i];
+		}
+	}
+}
+
+function getAreaByName(name) {
+	for (var i in areas) {
+		var area=areas[i];
+		if (area.name == name) {
+			return area;
+		}
+	}
+}
+
+function addToKnownUndiscoveredUndiscoveredWays(exceptId) {
+	var area = Object.assign({}, getAreaByName(currentArea));
+	area.exits = area.exits.filter(exit => exit.id != exceptId);
+	console.log("  newly known area with exits "+JSON.stringify(area)+ " adding to " +JSON.stringify(mapsWithKnownUndiscoveredWays));
+	mapsWithKnownUndiscoveredWays.push(area);
+}
+
+function getShorterPathToKnownUndiscoveredWays(from) {
+	var found;
+	console.log("    finding shorter known path to area with undiscovered ways from  " + from);
+	for (var i in mapsWithKnownUndiscoveredWays) {
+		if (mapsWithKnownUndiscoveredWays[i].exits.length==0) {
+			continue;
+		}
+		var key=from+"-"+mapsWithKnownUndiscoveredWays[i].name;
+		var path = knownPaths[key];
+		if (path) {
+			if (!found || found.length>path.length) {
+				console.log("    setting path "+key+ " from "+ JSON.stringify(found) +" to " + JSON.stringify(path));
+				found = path;
+			}
+		}
+	}
+
+	if (path) {
+		console.log("    nearest path to area with known undiscovered ways from " + from + " " +JSON.stringify(path));
+	} else {
+		console.log("    don't know any area with known undiscovered ways - don't have anywhere to go anymore.");
+	}
+	
+	return path;
+}
+
+//When enter a door addKnownPath: 
+function addKnownPath(previousArea, way) {
+	var addedNew = false;
+
+	//1.add direct know Path with only one step from-to for current movement
+	var key=previousArea+"-"+way.dest;
+	console.log("addKnownPath direct " + key + " " + JSON.stringify([way]));
+	if (!knownPaths[key] || knownPaths[key].length>1) {
+		addedNew = true;
+	}
+	knownPaths[key] = [way];
+
+	//2.make list of proposals (for all knownPaths, 
+	//   if your destination is in origin, will propose adding a extended new known path adding to beginning,
+	//   if your origin is in end of known path, propose a new extended added to end)
+	var proposals = [];
+	for (var i in knownPaths) {
+		if (!knownPaths[i].slice) {
+			console.log("ERROR - no function "+JSON.stringify(knownPaths));
+		}
+		var pathCopy = knownPaths[i].slice();
+		if (i.startsWith(way.dest + "-")) {
+			pathCopy.unshift(way);
+			var b = i.split("-")[1];
+			var key=""+previousArea+"-"+b;
+			if (b != previousArea) {
+				proposal = {key: key, path: pathCopy};
+				proposals.push(proposal);
+			}
+		}
+		if (i.endsWith("-" + previousArea)) {
+			pathCopy.push(way);
+			var a = i.split("-")[0];
+			var key=a+"-"+way.dest;
+			if (a != way.dest) {
+				proposal = {key: key, path: pathCopy};
+				proposals.push(proposal);
+			}
+		}
+	}
+	//["origin"-"destination": [{"dest":"hidden", "id":"2"}]]
+	//3.for each proposal, get using key, from knowPaths, if there is null or your is shorter, set it.
+	for (var i in proposals) {
+		var key = proposals[i].key;
+		var path = proposals[i].path;
+		if (!knownPaths[key] || knownPaths[key].length > path.length) {
+			console.log("addKnownPath by better proposal " + key + " " + JSON.stringify(path));
+			console.log(" known one was " + JSON.stringify(knownPaths[key]));
+			knownPaths[key] = path;
+			addedNew = true;
+		}
+	}
+
+	if (addedNew) {
+		console.log("  should try to add more known paths, since it was just added a new one.");
+	} else {
+		console.log("  should stop trying to add more known paths, since nothing was found now.");
+	}
+	return addedNew;
+}
+
+var steps = 0;
+
+while(steps ++ < 100) {
+
+	console.log("Location " + currentArea);
+
+	//1-If map where you are isn't here mapsWithKnownUndiscoveredWays, add it with all it's ways.
+	//If (map where you are is desiredDestination) desiredDestination=null;
+	if (!getKnownUndiscoveredWaysByName(currentArea)) {
+		addToKnownUndiscoveredUndiscoveredWays(enteredFromId);
+	}
+	if (currentArea == desiredDestination) {
+		desiredDestination=null;
+	}
+
+	var chooseDestination = false;
+	var choosenWay;
+	//2-if (!desiredDestination) and there is some way in mapsWithKnownUndiscoveredWays for this map, choosenWay=first unknown door In the list, prefer bidirectional or go exitOnly. If there is no way chooseDestination=true;
+	if (!desiredDestination) {
+		var knownUndiscoveredWays = getKnownUndiscoveredWaysByName(currentArea);
+		if (knownUndiscoveredWays) {
+			choosenWay = knownUndiscoveredWays.exits[0];
+		}
+
+		if (!choosenWay) {
+			chooseDestination=true;
+		}
+	//prefer bidirectional or go exitOnly;
+	}
+
+	//3-if (chooseDestination) choosenWay=entry from mapsWithKnownUndiscoveredWays with smaller item knownPaths from your point. If there is no entry to choose. The walk is finished.
+	if (chooseDestination && currentArea) {
+		var path = getShorterPathToKnownUndiscoveredWays(currentArea);
+		if (path) {
+			console.log("Got to go by " + JSON.stringify(path));
+			choosenWay = path[0];
+			desiredDestination = choosenWay.dest;
+		} else {
+			console.log("Finished walk " + JSON.stringify(walkPath));
+			console.log("  left to explore " + JSON.stringify(mapsWithKnownUndiscoveredWays));
+			console.log("  knownPaths " + JSON.stringify(knownPaths));
+			console.log("  walk path " + JSON.stringify(walkPath));
+
+			break;
+		}
+	}
+
+	//5-console.log the move
+	console.log(" moving to " + choosenWay.dest + " by " + choosenWay.id);
+	walkPath.push(choosenWay);
+
+	//4-Remove the way you've chosen mapsWithKnownUndiscoveredWays from here in the map where you are. If the map gets empty, remove it from mapsWithKnownUndiscoveredWays
+	var knownUndiscoveredWays = getKnownUndiscoveredWaysByName(currentArea);
+	if (knownUndiscoveredWays) {
+		knownUndiscoveredWays.exits = knownUndiscoveredWays.exits.filter(function isDestination(thisExit) {
+			return thisExit.dest != choosenWay.dest && thisExit.id != choosenWay.id;
+		});
+		console.log("   removing " +JSON.stringify(choosenWay) +" -> " + JSON.stringify(knownUndiscoveredWays));
+		getKnownUndiscoveredWaysByName(currentArea)
+	}
+
+	//6-Move to next map
+	var previousArea = currentArea;
+	currentArea = choosenWay.dest;
+	enteredFromId = choosenWay.id;
+
+	//7-addKnownPath
+	var addingNew = 200;
+	while (addingNew-->0 && addKnownPath(previousArea, choosenWay)) {};
+
+	if (addingNew == 0) {
+		console.log("ERROR - was in loop searching for new know paths seems to be always adding new indefinitely - the script believes it should keep trying to find more.");
+	}
+
+}
