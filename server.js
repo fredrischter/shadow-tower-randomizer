47f618daf445063bc4bf482ec9db2fe0337d816a
Fredo <fredrischter@gmail.com>
2025-03-14 21:00:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');(
+const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const { Storage } = require('@google-cloud/storage');
